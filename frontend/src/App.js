a4f1a77608d63795dcd9de9e76ced4b2cf10ce20
saikowshik384-maker <saikowshik384@gmail.com>
2026-05-18 12:36:28 +0530

    connected frontend with backend data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,24 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 function App() {
+
+  const [message, setMessage] = useState("");
 
   useEffect(() => {
     fetch("https://myproject-backend-x6p5.onrender.com/api")
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
-        alert(data.message);
+        setMessage(data.message);
       });
   }, []);
 
   return (
-    <div>
-      <h1>Frontend Connected Successfully 🚀</h1>
+    <div style={{ padding: "30px" }}>
+      <h1>Village Search System 🚀</h1>
+
+      <h2>Backend Message:</h2>
+
+      <p>{message}</p>
     </div>
   );
 }
