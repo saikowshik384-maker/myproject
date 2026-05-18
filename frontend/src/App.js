90e6fec4dff4f478f712c08a1b3820363e120bef
saikowshik384-maker <saikowshik384@gmail.com>
2026-05-18 12:53:27 +0530

    added village dropdown

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,24 +4,28 @@ function App() {
 
   const [state, setState] = useState("");
   const [district, setDistrict] = useState("");
+  const [village, setVillage] = useState("");
 
   const data = {
-    "Andhra Pradesh": [
-      "Krishna",
-      "Guntur",
-      "NTR"
-    ],
-    Telangana: [
-      "Hyderabad",
-      "Warangal",
-      "Karimnagar"
-    ]
+    "Andhra Pradesh": {
+      Krishna: ["Vijayawada", "Machilipatnam", "Gudivada"],
+      Guntur: ["Tenali", "Mangalagiri", "Sattenapalli"],
+      NTR: ["Ibrahimpatnam", "Tiruvuru", "Kanchikacherla"]
+    },
+
+    Telangana: {
+      Hyderabad: ["Madhapur", "Gachibowli", "Kukatpally"],
+      Warangal: ["Hanamkonda", "Kazipet", "Parkal"],
+      Karimnagar: ["Jammikunta", "Huzurabad", "Manakondur"]
+    }
   };
 
   return (
     <div style={{ padding: "30px" }}>
 
       <h1>Village Search System 🚀</h1>
+
+      {/* STATE */}
 
       <h2>Select State</h2>
 
@@ -30,6 +34,7 @@ function App() {
         onChange={(e) => {
           setState(e.target.value);
           setDistrict("");
+          setVillage("");
         }}
         style={{
           padding: "10px",
@@ -46,13 +51,18 @@ function App() {
         ))}
       </select>
 
+      {/* DISTRICT */}
+
       <h2 style={{ marginTop: "30px" }}>
         Select District
       </h2>
 
       <select
         value={district}
-        onChange={(e) => setDistrict(e.target.value)}
+        onChange={(e) => {
+          setDistrict(e.target.value);
+          setVillage("");
+        }}
         style={{
           padding: "10px",
           width: "300px",
@@ -62,19 +72,51 @@ function App() {
         <option value="">Choose District</option>
 
         {state &&
-          data[state].map((item, index) => (
+          Object.keys(data[state]).map((item, index) => (
             <option key={index} value={item}>
               {item}
             </option>
           ))}
       </select>
 
+      {/* VILLAGE */}
+
       <h2 style={{ marginTop: "30px" }}>
+        Select Village
+      </h2>
+
+      <select
+        value={village}
+        onChange={(e) => setVillage(e.target.value)}
+        style={{
+          padding: "10px",
+          width: "300px",
+          fontSize: "18px"
+        }}
+      >
+        <option value="">Choose Village</option>
+
+        {state &&
+          district &&
+          data[state][district].map((item, index) => (
+            <option key={index} value={item}>
+              {item}
+            </option>
+          ))}
+      </select>
+
+      {/* RESULTS */}
+
+      <h2 style={{ marginTop: "40px" }}>
         Selected State: {state}
       </h2>
 
       <h2>
         Selected District: {district}
+      </h2>
+
+      <h2>
+        Selected Village: {village}
       </h2>
 
     </div>
