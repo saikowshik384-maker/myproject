0161a7e65d3beaccd4259a43d8f38aafdbb25816
saikowshik384-maker <saikowshik384@gmail.com>
2026-05-18 12:14:06 +0530

    added api route

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require("express");
-
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("Backend is running");
 });
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+app.get("/api", (req, res) => {
+  res.json({
+    message: "API working successfully"
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
